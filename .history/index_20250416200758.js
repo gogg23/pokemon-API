34dd4d945629e05024e07b34d3538271f12adba6
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-04-16 20:45:14 +0300

    commit

---
+++ .history/index_20250416200758.js
@@ -0,0 +1,25 @@
+// fetch = Function used for making HTTP requests to fetch resources.
+//              (JSON style data, images, files)
+//              Simplifies asynchronous data fetching in JavaScript and
+//              used for interacting with APIs to retrieve and send
+//              data asynchronously over the web.
+//              fetch(url, {options})
+
+fetchData();
+async function fetchData(url) {
+  try {
+    const pokemonName = document.getElementById('pokemonName').value;
+    const response = await fetch(
+      'https://pokeapi.co/api/v2/pokemon/typhlosion'
+    );
+
+    if (!response.ok) {
+      throw new Error('Could not fetch resource');
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('error');
+  }
+}
